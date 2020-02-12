2e2af5ff6a2509d11a3307611617f73c78033342
raksupermarket <55552565+raksupermarket@users.noreply.github.com>
2020-02-12 13:01:46 +0700

    Update mesinkasir.js

--- src/pages/mesinkasir.js
+++ src/pages/mesinkasir.js
@@ -20,14 +20,14 @@ const MesinkasirPage = ({ data, location }) => {
 <p>Online technology adalah sebuah modern technology yang memungkinkan cek laporan dan monitoring pembukuan dimana dan kapan saja secara real time online merupakan generasi terbaru saat ini untuk kebutuhan mobilitas.</p>
 <p>Desktop offline mode adalah solusi terbaik pada usaha ini tanpa biaya bulanan dan dapat dipakai selamanya pada mesin kasir anda adalah prioritas utama dalam penggunaan nya, selain itu dengan based terbaik dan kompleks sangat stabil menggunakan system ini untuk kebutuhan stabilitas point of sale dalam penerapan nya.</p>
 <p>Apa saja POS <a href="mesinkasir" title="mesin kasir">mesin kasir</a> system untuk digunakan ?? kami menyediakan dua variant mesin kasir dalam penggunaan nya dan anda dapat menambahkan divisi sesuai dengan permintaan yang nanti nya akan kami kalkulasikan anggaran biaya pos point of sale sesuai kebutuhan pembagian divisi.</p>
-	  <img alt="mesin kasir" class="img-fluid rounded shadow shadow-lg" src="/img/TOUCHSCREEN6.png"/>
+	  <img alt="mesin kasir" class="img-fluid rounded shadow shadow-lg" src="https://raksupermarket.github.io/img/TOUCHSCREEN6.png"/>
 	  <h3><a href="mesinkasir" title="mesin kasir">TOUCHSCREEN POS</a></h3>
 	  <p>System all in one point of sale pos mesin kasir touchscreen persembahan dari ASUS untuk menunjang usaha, lengkap detail dan kompleks serta sangat sensitif dengan material desain mewah fashionable dengan slim desain ini adalah ciri modern mesin kasir untuk digunakan, tersedia beberapa package set plus injeksi software program kasir lengkap full support barcode dan touchscreen semakin sempurna dalam penggunaan nya, miliki ASUS all in one pos untuk usaha mu.</p>
-<a href="https://www.youtube.com/watch?v=NLxDbnyQ4r4" class="button" target="blank">Video</a>. <a href="/file/touchscreen.pdf" class="button" target="blank">Download</a>.
-	  <img alt="meja kasir terbaru" class="img-fluid rounded shadow shadow-lg" src="/img/mesinkasironline-murah.png"/>
+<a href="https://www.youtube.com/watch?v=NLxDbnyQ4r4" class="button" target="blank">Video</a>. <a href="https://raksupermarket.github.io/file/touchscreen.pdf" class="button" target="blank">Download</a>.
+	  <img alt="meja kasir terbaru" class="img-fluid rounded shadow shadow-lg" src="https://raksupermarket.github.io/img/mesinkasironline-murah.png"/>
 	  <h3><a href="mesinkasir" title="mesin kasir">PORTABLE POS</a></h3>
 	  <p>Portable Point of sale dengan offline mode semakin cepat dan tanpa biaya bulanan menjadikan solusi murah untuk pembukuan usaha mu, dukungan penuh by hewllet packard windows tablet 10inc dilengkapi dengan printer kasir bluetooth menjadikan kesempurnaan dalam aktivitas pembukuan dan transaksi semakin cepat , special untuk usaha kecil dan menengah dapat menggunakan technology mesin kasir murah ini, mewah dan elegan desain hingga kelengkapan akan pembukuan serta transaski menjadikan lebih baik untuk digunakan.</p>
-<a href="https://www.youtube.com/watch?v=cRKPNLRBTFU" class="button" target="blank">Video</a>. <a href="/file/portable.pdf" class="button" target="blank">Download</a>.
+<a href="https://www.youtube.com/watch?v=cRKPNLRBTFU" class="button" target="blank">Video</a>. <a href="https://raksupermarket.github.io/file/portable.pdf" class="button" target="blank">Download</a>.
 
 		   <hr/> <Bio /><hr/></Layout></div>
 			   <div class="col-12 col-md-3 p-3 p-md-5text-center"></div>
